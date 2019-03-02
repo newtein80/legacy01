f646559293b2566e2a41b1f1e9e00fd9a3740607
parkjongsoo <newtein80@gmail.com>
2019-03-02 14:09:24 +0900

    tutorial2 before Comment Add

--- src/main/java/com/lifesoft/legacy01/HomeController.java
+++ src/main/java/com/lifesoft/legacy01/HomeController.java
@@ -14,9 +14,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Handles requests for the application home page.
  */
-@Controller
+@Controller // 현재 클래스를 스크링에서 관리하는 컨트롤러로 등록(서버가 올라갈 때 스프링에서 HomeController를 자동으로 메모리에 올린다)
 public class HomeController {
 	
+	/*
+	 * @Controller : 컨트롤러 bean
+	 * @Repository : DAO(데이터베이스 관련 작업) bean
+	 * @Service : 서비스(비지니스 관련 로직) bean
+	 * @Inject : 의존관계 주입 bean
+	 */
+
 	/**
 	 * 로그를 수집할 클래스에 변수 선언
 	 * private: 외부에서 로그를 가로채지 못하도록 하기 위해서
@@ -26,6 +33,7 @@ public class HomeController {
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
+	 * 아래의 슬래쉬는 root를 의미 localhost/[site명]/ 을 의미
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
@@ -37,6 +45,11 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
+		/**
+		 * servlet-context.xml의 org.springframework.web.servlet.view.InternalResourceViewResolver을 참고하여
+		 * prefix, subfix에 규칙에 따라 페이지명으로 반환하도록함
+		 * --> 결국 forward 하는 동작을 하게 됨 
+		 */
 		
 		// servletContext 의 ViewResolver가 동작
 		return "home";
